d39bb677669c9db7e37234fcc9e6ea22c8fa9969
Joeven <marketing@rapidcleanillawarra.com.au>
2025-05-20 16:11:56 +0800

    Fix base path and routing configuration for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: '200.html',
 			precompress: false,
 			strict: true
 		}),

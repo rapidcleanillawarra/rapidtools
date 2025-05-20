e70b24f5c5c782f42c523de6b863fcba8194d2bd
Joeven <marketing@rapidcleanillawarra.com.au>
2025-05-20 15:19:28 +0800

    Fix base path configuration for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/rapidcleanillawarra' : ''
+			base: process.env.NODE_ENV === 'production' ? '/rapidcleanillawarra/' : ''
 		}
 	}
 };

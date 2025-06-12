bb209dc59df052c63df7ce4eae3d94ebd8edc149
Joeven <marketing@rapidcleanillawarra.com.au>
2025-06-12 14:36:47 +0800

    Fix 404 on update pricing

--- src/routes/update-product-pricing/+page.ts
+++ src/routes/update-product-pricing/+page.ts
@@ -1,5 +1,8 @@
 import type { PageLoad } from './$types';
 
+export const prerender = true;
+export const ssr = false;
+
 export const load: PageLoad = async ({ data }) => {
   return {
     products: data.products || []

c6f46cfcbb0cacd44f71386ebd74ae9f40a8f201
Joeven <marketing@rapidcleanillawarra.com.au>
2026-01-11 15:52:30 +0800

    fix: update button label from 'Generate' to 'Regenerate' in StatementAccountRow component for clarity

--- src/routes/(protected)/statement-of-accounts/components/StatementAccountRow.svelte
+++ src/routes/(protected)/statement-of-accounts/components/StatementAccountRow.svelte
@@ -60,7 +60,7 @@
 			on:click={handleGenerateDocument}
 			class="rounded bg-blue-600 px-3 py-1.5 text-xs font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
 		>
-			Generate
+			Regenerate
 		</button>
 	</td>
 	<td class="whitespace-nowrap px-6 py-4 text-sm">
